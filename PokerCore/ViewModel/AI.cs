ed73b8e8b43ce6258b260348e07f07e5c0c15990
Илья <MakarovIlya944@yandex.ru>
2019-02-23 19:03:26 +0700

    Добавил прототип интелекта

---
+++ PokerCore/ViewModel/AI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using ReactiveUI;
+
+namespace PokerCore.ViewModel
+{
+    class AI
+    {
+        private ITableStateForPlayer table;
+
+        private double TrustRatio = 0.5;
+
+        private double GetOuts()
+        {
+            //все карты, что усилят руку
+            return 0;
+        }
+
+        private double GetDiscountOuts()
+        {
+            //все карты, что усилят только нашу руку
+            return 0;
+        }
+
+        private double GetOdds()
+        {
+            //отношение бесполезных карт к аутам
+            return 0;
+        }
+
+        private double GetPotOdds()
+        {
+            //отношение текущего банка к текущей ставке
+            return 0;
+        }
+
+        private void MakeTurn()
+        {
+
+        }
+    }
+}
