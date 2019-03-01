085df17674f68393afceedea13ccfb342bf21698
Илья <MakarovIlya944@yandex.ru>
2019-03-02 01:23:03 +0700

    add working model, correct datacontext

--- PokerCore/Model/PokerM.cs
+++ PokerCore/Model/PokerM.cs
@@ -5,11 +5,25 @@ using ReactiveUI;
 
 namespace PokerCore.Model
 {
-    public class PokerM
+    public class PokerM : ITableStateForPlayer
     {
         List<Player> players;
         GameRules gameRules;
         TableStateForPlayer tableStateForPlayer;
+
+        public IEnumerable<IPlayerState> Players { get => players; }
+
+        public IEnumerable<ICard> BoardCards => throw new NotImplementedException();
+        int dealer;
+        public int Dealer { get => dealer; }
+        int currentRaise;
+        public int CurrentRaise { get => currentRaise; }
+        int bet;
+        public int Bet { get => bet; }
+        int bank;
+        public int Bank { get => bank; }
+        int bank2;
+        public int Bank2 { get => bank2; }
 
         public PokerM(string name, int maxplayer)
         {
