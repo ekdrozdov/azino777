7b9d80ca89e250db5e9da1102ca4b4600d2b39ee
Julia <Julia@JULIE>
2019-03-03 17:12:08 +0700

    add database init

---
+++ PokerCore/Model/DataBase/DBCard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerCore.Model.DataBase
+{
+    public class DBCard
+    {
+        public CardRank Rank { get; set; }
+        public CardSuit Suit { get; set; }
+    }
+}
---
+++ PokerCore/Model/DataBase/DBRound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerCore.Model.DataBase
+{
+    public class DBRound
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DBPlayerGameId { get; set; }
+        public DBPlayerGame PlayerGame { get; set; }
+
+        public string Action { get; set; }
+        public int BetSize { get; set; }
+        public DateTime DecisionTime { get; set; }
+    }
+}
