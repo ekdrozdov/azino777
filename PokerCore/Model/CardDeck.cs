04d084bcb1e501f1154b4aa772518929c1fb4511
Julia <Julia@JULIE>
2019-03-05 19:19:45 +0700

    add implementations in CardDeck.cs

--- PokerCore/Model/CardDeck.cs
+++ PokerCore/Model/CardDeck.cs
@@ -8,24 +8,46 @@ namespace PokerCore.Model
     {
         public CardDeck()
         {
+            _restCards = new List<Card>();
 
+            foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
+                {
+                    _restCards.Add(new Card(rank, suit));
+                }
+            }
+            Shuffle();
         }
 
         private List<Card> _restCards;
 
-        public List<ICard> RestCards { get; }
+        public List<Card> RestCards { get => _restCards; }
 
         public ICard TakeCard()
         {
-            Card Taked = _restCards[_restCards.Count - 1];
+            Card Taken = _restCards[_restCards.Count - 1];
             _restCards.RemoveAt(_restCards.Count - 1);
-            return Taked;
+            return Taken;
         }
 
         public void Shuffle()
         {
+            Card temp;
+            Random random = new Random();
+            int timesToShuffle = 100;
 
-            //_restCards = _restCards.Sort();
+            int cardToShuffle1, cardToShuffle2;
+
+            for (int i = 0; i < timesToShuffle; i++)
+            {
+                cardToShuffle1 = random.Next(_restCards.Count);
+                cardToShuffle2 = random.Next(_restCards.Count);
+
+                temp = _restCards[cardToShuffle1];
+                _restCards[cardToShuffle1] = _restCards[cardToShuffle2];
+                _restCards[cardToShuffle2] = temp;
+            }
         }
     }
 }
