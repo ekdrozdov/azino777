2a018c2a43685a34a12e7dda20fa1f42a41fa1bd
Илья <MakarovIlya944@yandex.ru>
2019-03-08 12:10:07 +0700

    Add cycle

---
+++ PokerCore/Model/CycleList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerCore.Model
+{
+    public class CycleListNode
+    {
+        int val { get; }
+        public CycleListNode Next { get; set; }
+        public CycleListNode(int _val, CycleListNode a)
+        {
+            val = _val;
+            Next = a;
+        }
+
+        public CycleListNode()
+        {
+
+        }
+    }
+
+    public class CycleList
+    {
+        List<CycleListNode> _data;
+
+        public CycleList()
+        {
+            _data = new List<CycleListNode>();
+        }
+
+        public void AddLast(int _val)
+        {
+            if (_data.Count != 0)
+            {
+                var a = new CycleListNode(_val, _data[0]);
+                _data[_data.Count - 1].Next = a;
+                _data.Add(a);
+            }
+            else
+            {
+                _data.Add(new CycleListNode(_val, new CycleListNode()));
+            }
+        }
+    }
+}
