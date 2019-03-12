8cabab2ac484c1a9f768e849f26c1b6c2c6cf7de
Julia <Julia@JULIE>
2019-03-12 12:03:33 +0700

    move GetTextureName in Card

--- PokerCore/ViewModel/Poker.cs
+++ PokerCore/ViewModel/Poker.cs
@@ -732,29 +732,5 @@ namespace PokerCore.Model
         {
             _players.Remove(key);
         }
-
-        public string GetTextureName(CardRank rank, CardSuit suit)
-        {
-            string name;
-            name = "card";
-
-            foreach (string suits in Enum.GetNames(typeof(CardSuit)))
-            {
-                if (suits.Equals(suit.ToString()))
-                {
-                    name += suits;
-                    foreach (string ranks in Enum.GetNames(typeof(CardRank)))
-                    {
-                        if (ranks.Equals(suit.ToString()))
-                        {
-                            name += ranks;
-                        }
-                    }
-                }
-            }
-
-            name += ".png";
-            return name;
-        }
     }
 }
